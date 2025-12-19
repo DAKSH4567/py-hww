5dcd2255d25f93efbd30088e3486a608a416627b
DAKSH4567 <dakshjain2122iva5@mpspnjpr.com>
2025-12-19 15:32:07 +0530

    initial commit

---
+++ vehicle/vehicle.py
@@ -0,0 +1,26 @@
+class BMW:
+    def start(self):
+        return "BMW engine started"
+
+    def speed(self):
+        return "BMW top speed is 250 km/h"
+
+
+class Ferrari:
+    def start(self):
+        return "Ferrari engine roars to life"
+
+    def speed(self):
+        return "Ferrari top speed is 340 km/h"
+
+
+def vehicle_info(vehicle):
+    print(vehicle.start())
+    print(vehicle.speed())
+
+
+bmw = BMW()
+ferrari = Ferrari()
+
+vehicle_info(bmw)
+vehicle_info(ferrari)
