e98094efa98ec1c7d255299dad64fd3fc335b68d
DAKSH4567 <dakshjain2122iva5@mpspnjpr.com>
2025-12-19 17:57:15 +0530

    initial commit

---
+++ trig/trigval.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+angle = random.randint(0, 360)
+radian = math.radians(angle)
+
+print("Angle:", angle)
+print("sin =", math.sin(radian))
+print("cos =", math.cos(radian))
+print("tan =", math.tan(radian))
